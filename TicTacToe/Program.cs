9de70576d77b73d614472385ff7f5e498893a3a4
danmalmx <danmalmx@gmail.com>
2018-12-07 19:42:28 +0100

    adding variables with positions corresponding to Array choices

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,8 +4,12 @@ namespace TicTacToe
 {
     class MainClass
     {
-        public static void Main(string[] args)
+
+        static 
+        public void Main(string[] args)
         {
+
+
             int[,] ticTacToeGrid =
             {
                 {1,2,3},
@@ -13,15 +17,25 @@ namespace TicTacToe
                 {7,8,9},
             };
 
+            int positionOne = ticTacToeGrid[0, 0];
+            int positionTwo = ticTacToeGrid[0, 1];
+            int positionThree = ticTacToeGrid[0, 2];
+            int positionFour = ticTacToeGrid[1, 0];
+            int positionFive = ticTacToeGrid[1, 1];
+            int positionSix = ticTacToeGrid[1, 2];
+            int positionSeven = ticTacToeGrid[2, 0]; 
+            int positionEight = ticTacToeGrid[2, 1];
+            int positionNine = ticTacToeGrid[2, 2];
+
             Console.Write("   |   |   |");
             Console.WriteLine("");
-            Console.WriteLine(" {0} | {1} | {2} |", ticTacToeGrid[0, 0], ticTacToeGrid[0, 1], ticTacToeGrid[0, 2]);
+            Console.WriteLine(" {0} | {1} | {2} |", positionOne, positionTwo, positionThree);
             Console.WriteLine("___|___|___|");
             Console.WriteLine("   |   |   |");
-            Console.WriteLine(" {0} | {1} | {2} |", ticTacToeGrid[1, 0], ticTacToeGrid[1, 1], ticTacToeGrid[1, 2]);
+            Console.WriteLine(" {0} | {1} | {2} |", positionFour, positionFive, positionSix);
             Console.WriteLine("___|___|___|");
             Console.WriteLine("   |   |   |");
-            Console.WriteLine(" {0} | {1} | {2} |", ticTacToeGrid[2, 0], ticTacToeGrid[2, 1], ticTacToeGrid[2, 2]);
+            Console.WriteLine(" {0} | {1} | {2} |", positionSeven, positionEight, positionNine);
             Console.WriteLine("___|___|___|");
 
         }
