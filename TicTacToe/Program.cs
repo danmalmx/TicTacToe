036c5da4afd757c88ebcf92c1303d270c112ccbc
danmalmx <danmalmx@gmail.com>
2018-12-07 12:39:36 +0100

    creating the TicTacToe grid, with a 2 dimensional array (3 x 3), and bounding lines

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,24 @@ namespace TicTacToe
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] ticTacToeGrid =
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+            };
+
+            Console.Write("   |   |   |");
+            Console.WriteLine("");
+            Console.WriteLine(" {0} | {1} | {2} |", ticTacToeGrid[0, 0], ticTacToeGrid[0, 1], ticTacToeGrid[0, 2]);
+            Console.WriteLine("___|___|___|");
+            Console.WriteLine("   |   |   |");
+            Console.WriteLine(" {0} | {1} | {2} |", ticTacToeGrid[1, 0], ticTacToeGrid[1, 1], ticTacToeGrid[1, 2]);
+            Console.WriteLine("___|___|___|");
+            Console.WriteLine("   |   |   |");
+            Console.WriteLine(" {0} | {1} | {2} |", ticTacToeGrid[2, 0], ticTacToeGrid[2, 1], ticTacToeGrid[2, 2]);
+            Console.WriteLine("___|___|___|");
+
         }
     }
 }
