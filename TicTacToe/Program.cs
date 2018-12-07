f2de91b4fc8982ed193d25dc74b1a2fbd4669870
danmalmx <danmalmx@gmail.com>
2018-12-07 09:53:55 +0100

    first push to development branch

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
